552a04edd2bdd18909364642bb6d5ee52cabd7eb
Sarthak Singh <sarthakankshawebjvb@gmail.com>
2025-06-03 17:48:28 +0530

    first Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import CatFact from './Comp/CatFact';
+
+const App = () => {
+  return (
+    <CatFact/>
+  )
+}
+
+export default App;
---
+++ frontend/src/Comp/CatFact.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function CatFact() {
+  const [fact, setFact] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const fetchCatFact = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get("https://catfact.ninja/fact");
+      setFact(response.data.fact);
+    } catch (error) {
+      setFact("Failed to fetch a cat fact.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Cat Fact</h1>
+      <button onClick={fetchCatFact}>
+        {loading ? "Loading..." : "Get a Cat Fact"}
+      </button>
+      {fact && <p style={{ marginTop: "20px" }}>{fact}</p>}
+    </div>
+  );
+}
+
+export default CatFact;
